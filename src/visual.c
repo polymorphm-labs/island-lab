aba0780c06425aca5acc4c1efc84667b1b0b731d
Andrei Antonov <polymorphm@gmail.com>
2019-09-28 14:28:07 +0300

    visual: using asprintf() instead of massive puting to a stream

--- src/visual.c
+++ src/visual.c
@@ -5,7 +5,7 @@
 // abort
 #include <stdlib.h>
 
-// fputc fprintf
+// asprintf fprintf
 #include <stdio.h>
 
 #include "util.h"
@@ -14,18 +14,11 @@
 #define UTL_S(name) island_lab_util_ ## name
 #define S(name) island_lab_visual_ ## name
 
-static void
-print_indent (FILE *stream, int indent)
-{
-    for (int i = 0; i < indent; ++i)
-    {
-        fputc (' ', stream);
-    }
-}
-
 static int
 calc_log (int num)
 {
+    // mathematical logarithm base 10 calculating
+
     if (num < 1)
     {
         abort ();
@@ -50,132 +43,248 @@ calc_log (int num)
 }
 
 static void
-print_rem_space_with_c (FILE *stream, int filled, char c)
+append_c (char **strp, char c)
+{
+    char *swp;
+    int res;
+
+    swp = *strp;
+    res = asprintf (strp, "%s%c", *strp, c);
+
+    if (res == -1)
+    {
+        abort ();
+    }
+
+    free (swp);
+}
+
+static void
+append_s (char **strp, const char *s)
+{
+    char *swp;
+    int res;
+
+    swp = *strp;
+    res = asprintf (strp, "%s%s", *strp, s);
+
+    if (res == -1)
+    {
+        abort ();
+    }
+
+    free (swp);
+}
+
+static void
+append_indent (char **strp, int indent)
+{
+    for (int i = 0; i < indent; ++i)
+    {
+        append_c (strp, ' ');
+    }
+}
+
+static void
+append_rem_space_with_c (char **strp, int filled, char c)
 {
     int cell_size = calc_log (UTL_S (general_size) ()) + 1;
 
     for (int i = 0; i < cell_size - filled; ++i)
     {
-        fputc (c, stream);
+        append_c (strp, c);
     }
 }
 
 static void
-print_rem_space (FILE *stream, int filled)
+append_rem_space (char **strp, int filled)
 {
-    print_rem_space_with_c (stream, filled, ' ');
+    append_rem_space_with_c (strp, filled, ' ');
 }
 
 static void
-print_num_cell (FILE *stream, int num)
+append_num_cell (char **strp, int num)
 {
     if (!num)
     {
-        print_rem_space (stream, 0);
+        append_rem_space (strp, 0);
         return;
     }
 
     int filled = calc_log (num);
+    char *swp;
+    int res;
 
-    fprintf (stream, "%d", num);
-    print_rem_space (stream, filled);
+    swp = *strp;
+    res = asprintf (strp, "%s%d", *strp, num);
+
+    if (res == -1)
+    {
+        abort ();
+    }
+
+    append_rem_space (strp, filled);
+
+    free (swp);
+}
+
+static void
+append_perimeter (char **strp, int *perimeter)
+{
+    char *swp;
+    int res;
+
+    for (int i = 0; i < UTL_S (perimeter_size) (); ++i)
+    {
+        swp = *strp;
+        if (i)
+        {
+            res = asprintf (strp, "%s,%d", *strp, perimeter[i]);
+        }
+        else
+        {
+            res = asprintf (strp, "%s%d", *strp, perimeter[i]);
+        }
+
+        if (res == -1)
+        {
+            abort ();
+        }
+
+        free (swp);
+    }
+}
+
+static void
+append_buildings (char **strp, int *buildings)
+{
+    char *swp;
+    int res;
+
+    for (int i = 0; i < UTL_S (buildings_size) (); ++i)
+    {
+        swp = *strp;
+
+        if (i)
+        {
+            res = asprintf (strp, "%s,%d", *strp, buildings[i]);
+        }
+        else
+        {
+            res = asprintf (strp, "%s%d", *strp, buildings[i]);
+        }
+
+        if (res == -1)
+        {
+            abort ();
+        }
+
+        free (swp);
+    }
 }
 
 void
 S (print_island) (FILE *stream, int *perimeter, int *buildings, int print_raw,
         int indent)
 {
-    if (print_raw) {
-        print_indent (stream, indent);
-        fprintf (stream, "perimeter raw seq: ");
-        for (int i = 0; i < UTL_S (perimeter_size) (); ++i)
-        {
-            if (i)
-            {
-                fputc (',', stream);
-            }
-            fprintf (stream, "%d", perimeter[i]);
-        }
-        fputc ('\n', stream);
+    char *str = calloc (1, sizeof (*str));
 
-        print_indent (stream, indent);
-        fprintf (stream, "buildings raw seq: ");
-        for (int i = 0; i < UTL_S (buildings_size) (); ++i)
-        {
-            if (i)
-            {
-                fputc (',', stream);
-            }
-            fprintf (stream, "%d", buildings[i]);
-        }
-        fputc ('\n', stream);
+    if (!str)
+    {
+        abort ();
     }
 
-    print_indent (stream, indent);
-    print_rem_space (stream, 0);
-    print_rem_space (stream, 0);
+    if (print_raw)
+    {
+        append_indent (&str, indent);
+        append_s (&str, "perimeter raw seq: ");
+        append_perimeter (&str, perimeter);
+        append_c (&str, '\n');
+
+        append_indent (&str, indent);
+        append_s (&str, "buildings raw seq: ");
+        append_buildings (&str, buildings);
+        append_c (&str, '\n');
+    }
+
+    append_indent (&str, indent);
+    append_rem_space (&str, 0);
+    append_rem_space (&str, 0);
     for (int i = 0; i < UTL_S (general_size) (); ++i)
     {
-        print_num_cell (stream, perimeter[UTL_S (north_obs_idx) (i)]);
+        append_num_cell (&str, perimeter[UTL_S (north_obs_idx) (i)]);
     }
-    fputc ('\n', stream);
+    append_c (&str, '\n');
 
-    print_indent (stream, indent);
-    print_rem_space (stream, 0);
-    fputc ('+', stream);
-    print_rem_space_with_c (stream, 1, '-');
+    append_indent (&str, indent);
+    append_rem_space (&str, 0);
+    append_c (&str, '+');
+    append_rem_space_with_c (&str, 1, '-');
     for (int i = 0; i < UTL_S (general_size) (); ++i)
     {
-        print_rem_space_with_c (stream, 0, '-');
+        append_rem_space_with_c (&str, 0, '-');
     }
-    fputc ('+', stream);
-    print_rem_space (stream, 1);
-    fputc ('\n', stream);
+    append_c (&str, '+');
+    append_rem_space (&str, 1);
+    append_c (&str, '\n');
 
     for (int j = 0; j < UTL_S (general_size) (); ++j)
     {
-        print_indent (stream, indent);
-        print_num_cell (stream, perimeter[UTL_S (west_obs_idx) (j)]);
-        fputc ('|', stream);
-        print_rem_space (stream, 1);
+        append_indent (&str, indent);
+        append_num_cell (&str, perimeter[UTL_S (west_obs_idx) (j)]);
+        append_c (&str, '|');
+        append_rem_space (&str, 1);
         for (int i = 0; i < UTL_S (general_size) (); ++i)
         {
-            print_num_cell (stream, buildings[UTL_S (building_idx) (j, i)]);
+            append_num_cell (&str, buildings[UTL_S (building_idx) (j, i)]);
         }
-        fputc ('|', stream);
-        print_rem_space (stream, 1);
-        print_num_cell (stream, perimeter[UTL_S (east_obs_idx) (j)]);
-        fputc ('\n', stream);
+        append_c (&str, '|');
+        append_rem_space (&str, 1);
+        append_num_cell (&str, perimeter[UTL_S (east_obs_idx) (j)]);
+        append_c (&str, '\n');
     }
 
-    print_indent (stream, indent);
-    print_rem_space (stream, 0);
-    fputc ('+', stream);
-    print_rem_space_with_c (stream, 1, '-');
+    append_indent (&str, indent);
+    append_rem_space (&str, 0);
+    append_c (&str, '+');
+    append_rem_space_with_c (&str, 1, '-');
     for (int i = 0; i < UTL_S (general_size) (); ++i)
     {
-        print_rem_space_with_c (stream, 0, '-');
+        append_rem_space_with_c (&str, 0, '-');
     }
-    fputc ('+', stream);
-    print_rem_space (stream, 1);
-    fputc ('\n', stream);
+    append_c (&str, '+');
+    append_rem_space (&str, 1);
+    append_c (&str, '\n');
 
-    print_indent (stream, indent);
-    print_rem_space (stream, 0);
-    print_rem_space (stream, 0);
+    append_indent (&str, indent);
+    append_rem_space (&str, 0);
+    append_rem_space (&str, 0);
     for (int i = 0; i < UTL_S (general_size) (); ++i)
     {
-        print_num_cell (stream, perimeter[UTL_S (south_obs_idx) (i)]);
+        append_num_cell (&str, perimeter[UTL_S (south_obs_idx) (i)]);
     }
-    fputc ('\n', stream);
+
+    fprintf (stream, "%s\n", str);
+
+    free (str);
 }
 
 void
 S (print_island_delim) (FILE *stream, int print_raw __attribute__ ((unused)),
         int indent)
 {
-    print_indent (stream, indent);
-    fputc ('\n', stream);
+    char *str = calloc (1, sizeof (*str));
+
+    if (!str)
+    {
+        abort ();
+    }
+
+    append_indent (&str, indent);
+
+    fprintf (stream, "%s\n", str);
+
+    free (str);
 }
 
 // vi:ts=4:sw=4:et
